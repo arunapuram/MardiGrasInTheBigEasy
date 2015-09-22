5ff7dc90d40462b6edf65d5cc8ba6e5a60109424
unknown4 <unknown4@297bb877-2d8a-4719-a40c-142a36202b2a>
2015-09-22 13:56:23 +0000

    * Updated Pick

--- MardiGrasInTheBigEasy/src/PickBonus.js
+++ MardiGrasInTheBigEasy/src/PickBonus.js
@@ -20,17 +20,82 @@ var PickBonusLayer = cc.Layer.extend({
 
         this.aPypBanner = this.pickscene.node.getChildByName("PickyourPoisonBanner");
         this.mathfunc = new PickMath();
-        this.mathfunc.initiate();
+        this.pick = this.mathfunc.shuffle();
+        this.pickAmount =  this.mathfunc.getPickObjectAwardAtIndex();
+        this.nCount = 0;
+        this.ntotCount = 5;
         //this.aPypBanner.visible = true;
         //PickyourPoisonBanner
         this.bEnable = false;
         var j = 0;
+
+        this.label = new cc.LabelTTF(this.ntotCount.toString(), "fonts/arial.ttf", 57);
+        this.addChild(this.label);
+        this.label.x = 517;//538.01;517
+        this.label.y = 39;// 599.08;51.77
+        this.label.setColor(cc.color(255,0,0,255));
+
+        this.creditsText = new cc.LabelTTF("15000", "fonts/arial.ttf", 57);
+        this.addChild(this.creditsText);
+        this.creditsText.x = 195;//538.01;517
+        this.creditsText.y = 39;// 599.08;51.77
+        this.creditsText.setColor(cc.color(255,0,0,255));
+
+        this.BetText = new cc.LabelTTF("150", "fonts/arial.ttf", 57);
+        this.addChild(this.BetText);
+        this.BetText.x = 710;//538.01;517
+        this.BetText.y = 37;// 599.08;51.77
+        this.BetText.setColor(cc.color(255,0,0,255));
+
+      /*  for(var nCount = 0; nCount < 12 ; nCount++) {
+            this.BetText = new cc.LabelTTF("150", "fonts/arial.ttf", 57);
+            this.addChild(this.BetText);
+            this.BetText.x = 710;//538.01;517
+            this.BetText.y = 37;// 599.08;51.77
+            this.BetText.setColor(cc.color(255, 0, 0, 255));
+        }*/
         for(var nCount = 0; nCount < 12 ; nCount++)
          {
             j = nCount+8;
             k = nCount+1;
             this.Pickbutton[nCount] = this.pickscene.node.getChildByName("Button_"+j);
             this.Coasterglow[nCount] = this.pickscene.node.getChildByName("Coasterglow"+k);
+             var expression = this.pick[nCount];
+
+             switch(expression) {
+                 case 0:
+                     var label = new cc.LabelTTF("margarita", "fonts/arial.ttf", 25);
+                     this.addChild(label);
+                     label.x = this.Pickbutton[nCount].x;
+                     label.y = this.Pickbutton[nCount].y;
+                     break;
+                 case 1:
+                     var label = new cc.LabelTTF("martini", "fonts/arial.ttf", 25);
+                     this.addChild(label);
+                     label.x = this.Pickbutton[nCount].x;
+                     label.y = this.Pickbutton[nCount].y;
+                     break;
+                 case 2:
+                     var label = new cc.LabelTTF("hurricane", "fonts/arial.ttf", 25);
+                     this.addChild(label);
+                     label.x = this.Pickbutton[nCount].x;
+                     label.y = this.Pickbutton[nCount].y;
+                     break;
+                 case 3:
+                     var label = new cc.LabelTTF("+3", "fonts/arial.ttf", 25);
+                     this.addChild(label);
+                     label.x = this.Pickbutton[nCount].x;
+                     label.y = this.Pickbutton[nCount].y;
+                     break;
+                 case 4:
+                     var label = new cc.LabelTTF("scotch", "fonts/arial.ttf", 25);
+                     this.addChild(label);
+                     label.x = this.Pickbutton[nCount].x;
+                     label.y = this.Pickbutton[nCount].y;
+                     break;
+                 default:
+                 break;
+             }
             this.pickobject[nCount] = [];
             for(var nIndex = 0; nIndex < 5 ; nIndex++)
             {
@@ -72,11 +137,31 @@ var PickBonusLayer = cc.Layer.extend({
                               if(this.button[nCount] == sender)
                               {
                                   this.nCurrentpickIndex = nCount;
+                                  this.nCount++;
+                                  this.ntotCount =  this.ntotCount -  1;
+                                  this.getPickObjectatIndex = this.mathfunc.getPickObjectatIndex(this.nCurrentpickIndex);
+                                  if (this.getPickObjectatIndex === 3)
+                                  {
+                                      this.ntotCount = this.ntotCount + 3;
+                                  }
                                   this.playCoasterFade();
                                   this.bEnable = true;
                                   this.playGlowBurst(sender.x,sender.y);
                                   sender.visible = false;
                                   sender.setEnabled(false);
+
+                                  //label.setAnchorPoint(cc.p(0, 0));
+                                  this.label._string = this.ntotCount.toString();
+                                  //this.label._originalText = "BYEEEEEEEEEEEEE";
+                                  this.label.x = 517;//538.01;517
+                                  this.label.y = 39;// 599.08;51.77
+                                  this.label.setColor(cc.color(255,0,0,255));
+
+
+                                /*  var label = new cc.LabelTTF("HIIIIIIIIIIIIIIIII"+  sender.visible, "fonts/arial.ttf", 57);
+                                  label.setAnchorPoint(cc.p(0, 0));
+                                  label.x = sender.x;
+                                  label.y = sender.y;*/
                               }
                               else
                               {
@@ -95,28 +180,32 @@ var PickBonusLayer = cc.Layer.extend({
                 this.pickobject[this.nCurrentpickIndex][this.PickObjecttype].visible = true;*/
             // create sprite sheet
             //this.bEnable = false;
-            var xyz = "Pickoneflip";
-            this.pickanimation = new SpriteAnimation(x,y,xyz,!true, this.playGlowBurstContinued.bind(this));
+            var str = "Pickoneflip";
+            this.pickanimation = new SpriteAnimation(x,y,str,!true, this.playGlowBurstContinued.bind(this));
             this.addChild(this.pickanimation.getNode(), 2);
                 this.pickanimation.playAnimationOnce();
             },
+
+
+
+
+
+
             playGlowBurstContinued: function (x,y) {
             // create sprite sheet
-                this.PickObjecttype = this.mathfunc.getPickObjectatIndex(this.nCurrentpickIndex);
-                 this.pickobject[this.nCurrentpickIndex][this.PickObjecttype].visible = true;
+            //    this.PickObjecttype = this.mathfunc.getPickObjectatIndex(this.nCurrentpickIndex);
+            this.pickobject[this.nCurrentpickIndex][this.getPickObjectatIndex].visible = true;
+            var label = new cc.LabelTTF(this.pickAmount[this.nCurrentpickIndex], "fonts/arial.ttf", 60);
+            this.pickscene.node.addChild(label,600);
+            label.x = x;
+            label.y = y;
+            label.setColor(cc.color(0,255,0,255));
             this.bEnable = false;
-             this.playCoasterFade();
-            var xyz = "glowburst";
-            var pickanimation2 = new SpriteAnimation(x,y, xyz, true);
+            this.playCoasterFade();
+            var str = "glowburst";
+            var pickanimation2 = new SpriteAnimation(x,y, str, true);
             this.pickscene.node.addChild(pickanimation2.getNode(),0);
             pickanimation2.playAnimation();
-
-
-                /*this.PickObjecttype = this.mathfunc.getPickObjectatIndex(this.nCurrentpickIndex);
-            this.pickobject[this.nCurrentpickIndex][this.PickObjecttype].visible = true;*/
-
-
-                //this.pickobject[3][2].visible = true;
             }
 
 
--- MardiGrasInTheBigEasy/src/PickMath.js
+++ MardiGrasInTheBigEasy/src/PickMath.js
@@ -1,41 +1,139 @@
 PickMath = function()
 {
-    this.getPickObjects = [];
-    this.aSymbolNames = [0,1,2,3,4];
-    this.aplus3Excluded = [0,1,2,4];
+    //this.getPickObjects = [];
+    //this.aSymbolNames = [0,1,2,3,4];
+    this.getPickObjects = [0,0,0,0,1,1,2,3,4,4,4,4];
+    this.aMargaritaAward = [300,300,450,450];
+    this.aScotchAward = [600,600,750,750];
+    this.TotalPickObjAward = [];
+    this.nMargaritaCounttt = 0;
+    this.nScotchCounttt = 0;
     this.bEnable = true;
-
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+PickMath.prototype.shuffle = function() {
+        var i = this.getPickObjects.length, j, swap;
+        while (--i) {
+            j = Math.random() * (i + 1) | 0;
+            swap = this.getPickObjects[i];
+            this.getPickObjects[i] = this.getPickObjects[j];
+            this.getPickObjects[j] = swap;
+        }
+        return this.getPickObjects;
+    }
 
 ////////////////////////////////////////////////////////////////////////////////////////
-PickMath.prototype.initiate = function()
-{
-    var sprite;
-    var nRandom;
-    for (var nCount = 0;nCount < 12 ; nCount++)
-    {
-        nRandom = Math.floor((Math.random() * 5));
-        sprite = (this.aSymbolNames[nRandom]);
-        if(sprite === 3 && this.bEnable === true)
-        {
-            this.bEnable = false;
-            this.getPickObjects[nCount] = [sprite];
-        }
-        else
-        {
-            nRandom = Math.floor((Math.random() * 4));
-            sprite = (this.aplus3Excluded[nRandom]);
-            this.getPickObjects[nCount] = [sprite];
-        }
+/*
 
+/!** pushRand
+
+ *!/
+    function pushRand (array, value, rng) {
+        var j = (rng || Math).random() * (array.length + 1) | 0;
+        array.push(array[j]);
+        array[j] = value;
+        return array.length;
     }
-};
+*/
+
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
 PickMath.prototype.getPickObjectatIndex = function(nCurrentindex)
 {
-      return this.getPickObjects[nCurrentindex][0];
+      return this.getPickObjects[nCurrentindex];
 };
 
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+PickMath.prototype.getMargaritaRandom = function()
+{
+    var i = this.aMargaritaAward.length, j, swap;
+    while (--i) {
+        j = Math.random() * (i + 1) | 0;
+        swap = this.aMargaritaAward[i];
+        this.aMargaritaAward[i] = this.aMargaritaAward[j];
+        this.aMargaritaAward[j] = swap;
+    }
+    return this.aMargaritaAward;
+}
+////////////////////////////////////////////////////////////////////////////////////////
+
+PickMath.prototype.getScotchRandom = function()
+{
+    var i = this.aScotchAward.length, j, swap;
+    while (--i) {
+        j = Math.random() * (i + 1) | 0;
+        swap = this.aScotchAward[i];
+        this.aScotchAward[i] = this.aScotchAward[j];
+        this.aScotchAward[j] = swap;
+    }
+    return this.aScotchAward;
+}
+////////////////////////////////////////////////////////////////////////////////////////
+
+PickMath.prototype.getPickObjectAwardAtIndex = function()
+{
+    this.getMargaritaRandom();
+    this.getScotchRandom();
+    for(var nCount = 0; nCount < 12 ; nCount++)
+    {
+        var expression = this.getPickObjects[nCount];
+        switch(expression) {
+            case 0:
+                //var label = new cc.LabelTTF("margarita", "fonts/arial.ttf", 25);
+                //this.addChild(label);
+                //label.x = this.Pickbutton[nCount].x;
+                //label.y = this.Pickbutton[nCount].y;
+                //  this.nMargaritaCounttt = 0;
+                //this.nScotchCounttt = 0;
+                //var
+                this.TotalPickObjAward[nCount] = this.aMargaritaAward[this.nMargaritaCounttt];
+                this.nMargaritaCounttt++;
+                break;
+            case 1:
+                //var label = new cc.LabelTTF("martini", "fonts/arial.ttf", 25);
+                //this.addChild(label);
+                //label.x = this.Pickbutton[nCount].x;
+                //label.y = this.Pickbutton[nCount].y;
+                this.TotalPickObjAward[nCount] = 1500;
+                break;
+            case 2:
+          /*      var label = new cc.LabelTTF("hurricane", "fonts/arial.ttf", 25);
+                this.addChild(label);
+                label.x = this.Pickbutton[nCount].x;
+                label.y = this.Pickbutton[nCount].y;*/
+                this.TotalPickObjAward[nCount] = 2350;
+                break;
+            case 3:
+            /*    var label = new cc.LabelTTF("+3", "fonts/arial.ttf", 25);
+                this.addChild(label);
+                label.x = this.Pickbutton[nCount].x;
+                label.y = this.Pickbutton[nCount].y;*/
+                this.TotalPickObjAward[nCount] = 0;
+                break;
+            case 4:
+       /*         var label = new cc.LabelTTF("scotch", "fonts/arial.ttf", 25);
+                this.addChild(label);
+                label.x = this.Pickbutton[nCount].x;
+                label.y = this.Pickbutton[nCount].y;*/
+
+                this.TotalPickObjAward[nCount] = this.getScotchRandom()[this.nScotchCounttt];
+                this.nScotchCounttt++;
+                break;
+            default:
+                break;
+        }
+    }
+    return this.TotalPickObjAward;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////
+//0 = margarita(Minimum 4 are present usually)
+//1 = martini(usually 2)
+//2 = hurricane(atleast one)
+//3 = +3(atleast one)
+//4 = scotch(atleast 4)
